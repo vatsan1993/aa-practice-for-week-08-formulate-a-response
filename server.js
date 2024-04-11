9a3b9eb1f9cb00e22213e1e09086d4995e287fe8
Srivatsan <vatsan1993@gmail.com>
2024-04-11 16:53:53 +0530

    finished program

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 // Your code here
+const http = require('http');
+const PORT = 3000;
+const httpServer = http.createServer((req, res) => {
+  // multi-line JavaScript string using a template literal
+  const responseBody = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World!</title>
+</head>
+<body>
+  <h1>Hello there!</h1>
+</body>
+</html>
+`;
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/html');
+  res.end(responseBody);
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
